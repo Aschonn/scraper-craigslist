181427886b2b77a52afbc35c46db599b85f0a28c
Aschonn <Aschonn_trinity@yahoo.com>
2020-07-19 13:14:36 -0500

    made scraper comments and made readme more human

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
 # scraper-craigslist
 
-A craigslist scraper that creates a json with information of posts.
+A craigslist scraper that creates a json file with postes from 
 
-instructions: 
-1) npm install puppeteer fs
-2) Enter custom values into directed areas
-3) enter into command line: node scraper.js
-4) finish 
+## Getting Started
+
+### `npm install puppeteer fs puppeteer-extra`
+This install all the needed dependencies.
+
+### Enter in custom search term and location
+
+### `node scraper.js`
+
+This starts the scraper
+
+
+## Mock Data Example
+### City = Fargo
+### Search = Motorcycle
+
+```
+{
+    title: 'Full Face Fulmer Helmet',
+    url: 'https://fargo.craigslist.org/mpo/d/vergas-full-face-fulmer-helmet/7160600625.html',
+    price: '$30'
+  },
+  {
+    title: 'hjc helmets',
+    url: 'https://fargo.craigslist.org/mpo/d/fargo-hjc-helmets/7160571651.html',
+    price: '$150'
+  },
+  {
+    title: 'Dunlop lite Tires',
+    url: 'https://fargo.craigslist.org/mpo/d/fargo-dunlop-lite-tires/7160571894.html',
+    price: '$300'
+  },
+  {
+    title: 'victory saddlebag liners',
+    url: 'https://fargo.craigslist.org/mpo/d/fargo-victory-saddlebag-liners/7160571375.html',
+    price: '$50'
+}
+  ```
+
+## Optional/More Information
+
+### Dont want webpage opening?
+turn `headless: false` to `headless : true`
+
+### Want JSON file to be a different name? 
+Go to `fs.writeFile` and change `'myjsonfile.json'` to `'AnythingYouLike.json'`
+
+
 
--- scraper.js
+++ scraper.js
@@ -5,47 +5,75 @@ const fs = require("fs");
 
 
 //Enter In A State:
-const city = "fargo"; //enter a value here
+const city = "fargo"; //ENTER CITY
 
+//our custom url
 const url = `https://${city}.craigslist.org`;
 
-
+//if there is an error send message
 main().then(() => console.log('Done')).catch(error => console.log("there was an error in your inputs please reload and enter valid inputs", error));
 
 async function main() {
+    const query = 'motorcycle';
+
+    //launches puppeteer
     const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+    
+    //gets a page
     const page = await browser.newPage();
+    
+    //goes to specified url
     await page.goto(url);
 
+    //give webpage time to load
     await page.waitFor("input[name='query']");
-
-
+    
+    // click on search 
     await page.click("input[name='query']");
+
+
+    
+    //enter term we want to search
     await page.evaluate(() => {
-        document.querySelector('input[name="query"]').value = "motorcycle"; //enter a value here
+        document.querySelector('input[name="query"]').value = 'Motorcycle'; //ENTER VALUE
     });
 
+    
+    
+    
+    //press enter after inputing search term
     await page.keyboard.press('Enter');
 
+    //waits for specific item to load
     await page.waitFor(".result-row");
 
+    //gather data
     const data = await page.$$eval(".result-row", rows => {
         return rows.map(row => {
+            
+            //creates dict so we can manipulate data easier
             const properties = {};
+            
+            //gather post title and url
             const titleElement = row.querySelector(".result-title");
+            //title
             properties.title = titleElement ? titleElement.innerText : "";
+            //url
             properties.url = titleElement ? titleElement.getAttribute("href") : "";
+            //gather price 
             const priceElement = row.querySelector('.result-price');
-            properties.price = priceElement ? priceElement.innerText : "";
-            const imageElement = row.querySelector(".swipe [data-index='0'] img");
-            properties.imageUrl = imageElement ? imageElement.getAttribute("src") : "";
+            properties.price = priceElement ? priceElement.innerText : ""
 
             return properties
         });
 
 
     });
+
+    //consoles data 
     console.log(data);
+
+    //writes json file for later use if needed
     fs.writeFile('myjsonfile.json', JSON.stringify(data), function (err, result) {
         if (err) console.log('error', err);
     });
